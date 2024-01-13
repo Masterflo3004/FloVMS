ab2257942bdb2515b9e5926aeb69043e4a4d3968
Masterflo3004 <100784148+Masterflo3004@users.noreply.github.com>
2024-01-13 14:00:00 +0000

    1

---
+++ TestVM/main.c
@@ -0,0 +1,18 @@
+/* ========NOTES=========
+Bits: 64
+Opcode 
+
+*/
+//-------Inports-----------
+#include <stdint.h>
+
+//-------defines-----------
+
+//--------code--------------
+
+
+int main(){
+    int32_t memory[__INT32_MAX__];
+    
+
+}
