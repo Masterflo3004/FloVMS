674c59c2cf4fd89e7df7f65386bb4f2959051354
Masterflo3004 <100784148+Masterflo3004@users.noreply.github.com>
2024-01-14 12:15:31 +0000

    1

--- TestVM/main.c
+++ TestVM/main.c
@@ -1,18 +1,81 @@
 /* ========NOTES=========
-Bits: 64
+Bits: 32
 Opcode 
-
+00000001000000010000000100001010
 */
 //-------Inports-----------
 #include <stdint.h>
-
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 //-------defines-----------
+#define OPC(i) ((i)>>24)
+#define inst1(i) (((i)<<8)>>24)
+#define inst2(i) (((i)<<16)>>24)
+#define inst3(i) (((i)<<24)>>24)
+#define instmem(i) (((i)<<16)>>16)
+
 
 //--------code--------------
+enum registers{r0=0, r1, r2, r3, r4, r5, r6, r7, r8, r9, RPC, RCND, RCNT};
+uint32_t reg[RCNT];
+int32_t memory[__UINT16_MAX__];
+uint32_t startPC = 0x3000;
+
+int ext(uint32_t i){
+    return 0;
+}
+int mov(uint32_t i){
+    reg[inst1(i)] = inst2(i);
+    return 1;
+}
+int add(uint32_t i){
+    reg[inst1(i)] = reg[inst2(i)] + reg[inst3(i)];
+    return 1;
+}
+int add2(uint32_t i){
+    reg[inst1(i)] = reg[inst2(i)] + inst3(i);
+    return 1;
+}
+int load(uint32_t i){
+    reg[inst1(i)]=memory[instmem(i)];
+    return 1;
+}
+int save(uint32_t i){
+    memory[instmem(i)]= reg[inst1(i)];
+    return 1;
+}
+
+
+typedef int (*opc_f)(uint32_t instruction);
+opc_f funcs[__UINT8_MAX__]={ext, mov, add, add2,load, save};
+
+
+void run(){
+    int running = 1;
+    reg[RPC]=startPC;
+    while(running){
+        int i = memory[reg[RPC]];
+        running = funcs[OPC(i)](i);
+        reg[RPC]++;
+    }
+    
+}
+
+void loadprogramm(char *filename){
+    FILE *file = fopen(*filename, "rb")
+}
+
 
 
 int main(){
-    int32_t memory[__INT32_MAX__];
+    char file[128]; 
+    printf("Enter Filename To load (max 128 characters)");
+    scanf("%s", file);
+    loadprogramm(file);
+    run();
+    printf("Programm run successfully");
+    printf("%d",reg[r5]);
+    return 1;
     
-
 }
