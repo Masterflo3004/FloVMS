49087253bf8e3f535e6628114db5afd9ed655bb9
Masterflo3004 <100784148+Masterflo3004@users.noreply.github.com>
2024-01-21 11:24:57 +0000

    1

---
+++ Blockchain/main.c
@@ -0,0 +1,17 @@
+/*
+
+------main function-------
+create memory
+starts udp socket,
+sets socket to nonblocking
+inputs join ip (IPv6)
+sends handshake to join ip
+while loop:
+    socket recv
+    append recv to tasks
+    send recv
+    check if something is in tasks
+    do task
+
+
+*/
